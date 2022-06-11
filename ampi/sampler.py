974b6be843d962975d21e83773fd5e170fa621cd
alienzj <alienchuj@gmail.com>
2022-06-11 15:36:49 +0800

    update parse_samples function

--- ampi/sampler.py
+++ ampi/sampler.py
@@ -10,14 +10,23 @@ def parse_samples(samples_tsv, reads_layout="PE", check_samples=False):
                    .set_index("sample-id", drop=False)
 
     cancel = False
-    if "forward-absolute-filepath" in samples_df.columns:
+
+    file_header = ""
+    if reads_layout == "PE":
+        file_header = "forward-absolute-filepath"
+    else:
+        file_header = "absolute-filepath"
+
+    if file_header in samples_df.columns:
         for sample_id in samples_df.index.unique():
             if "." in sample_id:
                 print(f"{sample_id} contain '.', please remove '.', now quiting :)")
                 cancel = True
 
-            fq1_list = samples_df.loc[[sample_id], "forward-absolute-filepath"].dropna().tolist()
-            fq2_list = samples_df.loc[[sample_id], "reverse-absolute-filepath"].dropna().tolist()
+            fq1_list = samples_df.loc[[sample_id], file_header].dropna().tolist()
+            if reads_layout == "PE":
+                fq2_list = samples_df.loc[[sample_id], file_header.replace("forward", "reverse")].dropna().tolist()
+
             for fq_file in fq1_list:
                 if not fq_file.endswith(".gz"):
                     print(f"{fq_file} need gzip format")
@@ -26,10 +35,17 @@ def parse_samples(samples_tsv, reads_layout="PE", check_samples=False):
                     if not os.path.exists(fq_file):
                         print(f"{fq_file} not exists")
                         cancel = True
-                    if reads_layout == "PE":
-                        if len(fq2_list) == 0:
-                            print(f"{sample_id} fq2 not exists")
+
+            if reads_layout == "PE":
+                for fq_file in fq2_list:
+                    if not fq_file.endswith(".gz"):
+                        print(f"{fq_file} need gzip format")
+                        cancel = True
+                    if check_samples:
+                        if not os.path.exists(fq_file):
+                            print(f"{fq_file} not exists")
                             cancel = True
+
     else:
         print("wrong header: {header}".format(header=samples_df.columns))
         cancel = True
